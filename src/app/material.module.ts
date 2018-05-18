03d38600017b7f48d22748951abfbd72c5e4fe25
Arek Ruta <arek.ruta89@gmail.com>
2018-05-18 11:10:36 +0200

    theme.scssm flex-layout,

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,10 @@
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
-  imports: [CommonModule, MatButtonModule],
-  exports: [CommonModule, MatButtonModule],
+  imports: [MatButtonModule, FlexLayoutModule],
+  exports: [MatButtonModule, FlexLayoutModule],
   declarations: []
 })
 export class MaterialModule {}
